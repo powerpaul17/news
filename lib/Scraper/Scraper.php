148c0f95316a37eeb4194802f0c1c32a9d9fd956
Paul Tirk <paultirk@Aurora2.goodsrv.de>
2020-02-03 19:39:00 +0100

    Revert "Allow getContent() in Scraper and IScraper to return null (#606)"
    This reverts commit d27dddad25b1333f531377b5699e0be8fa1896ea.

--- lib/Scraper/Scraper.php
+++ lib/Scraper/Scraper.php
@@ -82,7 +82,7 @@ class Scraper implements IScraper
         return true;
     }
 
-    public function getContent(): ?string
+    public function getContent(): string
     {
         if ($this->readability === null) {
             return null;
