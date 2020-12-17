c1eef7be323bf48233814f3399123f01ac7ec3dd
Paul Tirk <paultirk@paultirk.com>
2020-12-17 22:16:56 +0000

    change build config to support typescript

--- webpack.js
+++ webpack.js
@@ -1,19 +1,73 @@
-const { merge } = require('webpack-merge')
+const path = require('path')
 const webpack = require('webpack')
-const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-const config = {
-    module: {
-        rules: [
-            {
-				test: /\.(png|jpg|gif|svg)$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]?[hash]',
-				},
-			},
-        ],
-    },
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const appName = process.env.npm_package_name
+const appVersion = process.env.npm_package_version
+const buildMode = process.env.NODE_ENV
+const isDev = buildMode === 'development'
+console.info('Building', appName, appVersion, '\n')
+
+module.exports = {
+  mode: buildMode,
+  devtool: isDev ? '#cheap-source-map' : '#source-map',
+  entry: {
+    main: path.resolve(path.join('src', 'main.js')),
+  },
+  output: {
+    path: path.resolve('./js'),
+    publicPath: '/js/',
+    filename: `${appName}-[name].js?v=[contenthash]`,
+    chunkFilename: `${appName}-[name].js?v=[contenthash]`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new StyleLintPlugin({
+      files: 'src/**/*.{css,scss,vue}',
+    }),
+    new ESLintPlugin(),
+
+    // Make appName & appVersion available as a constant
+    new webpack.DefinePlugin({ appName: JSON.stringify(appName) }),
+    new webpack.DefinePlugin({ appVersion: JSON.stringify(appVersion) }),
+  ],
+  resolve: {
+    extensions: ['*', '.ts', '.js', '.vue'],
+    symlinks: false,
+  },
 }
-
-module.exports = merge(config, webpackConfig)
